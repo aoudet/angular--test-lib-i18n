046eca6a813cc03317086bb6032be420173bc2e5
aoudet <aoudet@users.noreply.github.com>
2022-09-19 08:58:16 -0400

    16 files deleted

--- dist/i18n-test/lib/weather/enums/weather-enum.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum WeatherEnum {
-    none = 0,
-    sun = 1,
-    cloud = 2,
-    rain = 4,
-    snow = 8,
-    variable = 16
-}
--- dist/i18n-test/public-api.d.ts
+++
@@ -1,4 +0,0 @@
-import '@angular/localize/init';
-export * from './lib/weather/weather.component';
-export * from './lib/weather/enums/weather-enum';
-export * from './lib/i18n-test.module';
